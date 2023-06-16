fab96d69290781bda7f0ea999cf35e40d75272da
Ashwanikumarshaw <Ashwanikumar11shaw@gmail.com>
2023-06-16 12:14:09 +0530

    set the name of thread

--- src/MultithreadingClasses/Thread1.java
+++ src/MultithreadingClasses/Thread1.java
@@ -1,11 +1,15 @@
 package MultithreadingClasses;
 
-public class Thread1 extends Thread{
+public class Thread1 extends Thread {
 	
+	public Thread1(String name) {
+		super(name);//set the name of thread 
+	}
+
 	@Override
 	public void run() {
-		for(int i=0;i<50;i++)
-			System.out.println("Thread1 running "+i);
+		for (int i = 0; i < 50; i++)
+			System.out.println(Thread.currentThread().getName()+" running " + i);
 	}
 
 }
--- src/MultithreadingClasses/threadTest.java
+++ src/MultithreadingClasses/threadTest.java
@@ -5,7 +5,7 @@ public class threadTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("main start");
-		Thread t1=new Thread1();
+		Thread t1=new Thread1("thread 1 ");
 //		t1.setDaemon(true);//might not allow to run all thread 
 		t1.start();
 		System.out.println("main end");
