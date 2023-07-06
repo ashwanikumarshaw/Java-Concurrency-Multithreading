3c313ac25b40a54f31cf3d30bcab0c94ad815bfb
Ashwanikumarshaw <Ashwanikumar11shaw@gmail.com>
2023-07-06 11:03:20 +0530

    Runnable interface by implementing using lamda

--- src/MultithreadingClasses/threadTest.java
+++ src/MultithreadingClasses/threadTest.java
@@ -9,10 +9,21 @@ public class threadTest {
 //		t1.setDaemon(true);//might not allow to run all thread 
 		t1.start();
 		System.out.println("main end");
+		
+		
 		//order will mixed up & decided by cpu
 		System.out.println("main using Runnable start");
 		Thread t2=new Thread(new Thread2(),"thread 2 ");
 		t2.start();
+		System.out.println("main using Runnable end");
+		
+		
+		System.out.println("main using Runnable interface by implementing using lamda start");
+		Thread t3=new Thread(()->{
+			for(int i=0;i<5;i++)
+				System.out.println(Thread.currentThread()+" , "+i);
+		},"thread 3 ");
+		t3.start();
 		System.out.println("main using Runnable end");
 		
 	}
